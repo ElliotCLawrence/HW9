f575b0126b3fa2fa67f64e1a32a6ffb491a34d9f
elliotclawrence <elliotclawrence@gmail.com>
2016-10-21 22:25:44 -0700

    Fixed FileSystemDefs.cs by adding the new version from HW8

--- HW3 Test/FileSystemDefs.cs
+++ HW3 Test/FileSystemDefs.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿//Elliot Lawrence
+//HW8
+//CS 422
+//10_21_2016
+
+
+
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -36,9 +44,9 @@ namespace CS422
 
     public abstract class File422
     {
-        public string Name { get; }
+        public abstract string Name { get; }
 
-        public Dir422 Parent { get; }
+        public abstract Dir422 Parent { get; }
 
         public abstract Stream OpenReadOnly();
 
@@ -107,15 +115,15 @@ namespace CS422
             }
         }
 
-        public override bool ContainsDir(string dirName, bool recursive)
+        public override bool ContainsDir(string dirName, bool recursive) //checks if the directory contains the directory
         {
-            if (dirName.Contains("/") || dirName.Contains("\\"))
+            if (dirName == null || dirName == "" || dirName.Contains("/") || dirName.Contains("\\"))
             {
                 return false;
             }
 
 
-            foreach (string dir in Directory.GetDirectories(m_path))
+            foreach (string dir in Directory.GetDirectories(m_path)) 
             {
                 if (Path.GetDirectoryName(dir) == dirName)
                 {
@@ -135,8 +143,11 @@ namespace CS422
             return false;
         }
 
-        public override bool ContainsFile(string fileName, bool recursive)
+        public override bool ContainsFile(string fileName, bool recursive)//checks if the directory contains the file
         {
+            if (fileName == null || fileName == "" || fileName.Contains("/") || fileName.Contains("\\"))
+                return false;
+
             foreach (string file in Directory.GetFiles(m_path))
             {
                 if (Path.GetFileName(file) == fileName)
@@ -145,8 +156,11 @@ namespace CS422
             return false;
         }
 
-        public override Dir422 CreateDir(string name)
+        public override Dir422 CreateDir(string name)//creates a directory and gives back a copy
         {
+            if (name == null || name == "" || name.Contains("/") || name.Contains("\\"))
+                return null;
+
             string fullName = m_path+"/"+name;
             if (Directory.CreateDirectory(fullName) != null)
                 return new StdFSDir(fullName);
@@ -155,8 +169,11 @@ namespace CS422
 
         }
 
-        public override File422 CreateFile(string name)
+        public override File422 CreateFile(string name)//creates a file and gives back a copy
         {
+            if (name == null || name == "" || name.Contains("/") || name.Contains("\\"))
+                return null;
+
             string fullName = m_path + "/" + name;
             if (File.Create(fullName) != null)
                 return new StdFSFile(fullName);
@@ -164,7 +181,7 @@ namespace CS422
                 return null;
         }
 
-        public override Dir422 getDir(string name)
+        public override Dir422 getDir(string name)//gets a directory if it exists in current directory
         {
             string fullName = m_path + "/" + name;
             foreach (string dir in Directory.GetDirectories(m_path))
@@ -176,7 +193,7 @@ namespace CS422
                 return null;
         }
 
-        public override IList<Dir422> GetDirs()
+        public override IList<Dir422> GetDirs()//gets a directory if it exists in current directory
         {
             List<Dir422> dirs = new List<Dir422>();
             foreach (string dir in Directory.GetDirectories(m_path))
@@ -186,7 +203,7 @@ namespace CS422
             return dirs;
         }
 
-        public override File422 GetFile(string name)
+        public override File422 GetFile(string name)//returns a file if it exists in current directory
         {
             string fullName = m_path + "/" + name;
             foreach (string file in Directory.GetFiles(m_path))
@@ -198,7 +215,7 @@ namespace CS422
             return null;
         }
 
-        public override List<File422> GetFiles()
+        public override List<File422> GetFiles() //returns all files in current directory
         {
             List<File422> files = new List<File422>();
             foreach (string file in Directory.GetFiles(m_path))
@@ -214,6 +231,22 @@ namespace CS422
         private string m_path;
 
         public StdFSFile(string path) { m_path = path; }
+
+        public override string Name
+        {
+            get
+            {
+                return Path.GetFileName(m_path);
+            }
+        }
+
+        public override Dir422 Parent
+        {
+            get
+            {
+                return new StdFSDir(Directory.GetParent(m_path).FullName);
+            }
+        }
 
         public override Stream OpenReadOnly() //one line function return a stream with m_path in it
         {
@@ -251,7 +284,7 @@ namespace CS422
         private List<File422> fileChildren;
         private string name;
 
-        public MemFSDir(string name, MemFSDir parentD)
+        public MemFSDir(string name, MemFSDir parentD) //this is the constructor for this class
         {
             this.name = name;
             dirParent = parentD;
@@ -259,7 +292,7 @@ namespace CS422
             fileChildren = new List<File422>();
         }
 
-        public override string Name
+        public override string Name //getter for name
         {
             get
             {
@@ -267,7 +300,7 @@ namespace CS422
             }
         }
 
-        public override Dir422 Parent
+        public override Dir422 Parent //getter for parent
         {
             get
             {
@@ -275,8 +308,13 @@ namespace CS422
             }
         }
 
-        public override bool ContainsDir(string dirName, bool recursive)
+        public override bool ContainsDir(string dirName, bool recursive) //checks if the directory contains the child
         {
+
+            if (dirName == null || dirName == "" || dirName.Contains("/") || dirName.Contains("\\"))
+                return false;
+
+
             foreach (MemFSDir child in directoryChildren) //check all the files in this folder for a match
             {
                 if (child.Name == dirName)
@@ -295,8 +333,11 @@ namespace CS422
             return false;
         }
 
-        public override bool ContainsFile(string fileName, bool recursive)
+        public override bool ContainsFile(string fileName, bool recursive) //checks if the directory contains the file
         {
+            if (fileName == null || fileName == "" || fileName.Contains("/") || fileName.Contains("\\"))
+                return false;
+
             foreach (MemFSFile child in fileChildren) //check all the files in this folder for a match
             {
                 if (child.Name == fileName)
@@ -315,20 +356,23 @@ namespace CS422
             return false;
         }
 
-        public override Dir422 CreateDir(string name)
+        public override Dir422 CreateDir(string name) //creates a directory and gives back a copy
         {
+            if (name == null || name == "" || name.Contains("/") || name.Contains("\\"))
+                return null;
             directoryChildren.Add(new MemFSDir(name, this));
             return directoryChildren[directoryChildren.Count-1];
         }
 
-        public override File422 CreateFile(string name)
+        public override File422 CreateFile(string name) //creates a file and gives back a copy
         {
-            
+            if (name == null || name == "" || name.Contains("/") || name.Contains("\\"))
+                return null;
             fileChildren.Add(new MemFSFile(name, this));
             return fileChildren[fileChildren.Count - 1];
         }
 
-        public override Dir422 getDir(string name)
+        public override Dir422 getDir(string name) //gets a directory if it exists in current directory
         {
             for (int x = 0; x < directoryChildren.Count; x++)
             {
@@ -339,12 +383,12 @@ namespace CS422
             return null;
         }
 
-        public override IList<Dir422> GetDirs()
+        public override IList<Dir422> GetDirs() //returns all directories in current directory
         {
             return directoryChildren;
         }
 
-        public override File422 GetFile(string name)
+        public override File422 GetFile(string name) //returns a file if it exists in current directory
         {
             for (int x = 0; x < fileChildren.Count; x++)
             {
@@ -355,7 +399,7 @@ namespace CS422
             return null;
         }
 
-        public override List<File422> GetFiles()
+        public override List<File422> GetFiles() //returns all files in current directory
         {
             return fileChildren;
         }
@@ -365,13 +409,28 @@ namespace CS422
     {
         private string fileName;
         MemFSDir parentDir;
+        List<trackingMemStream> readers;
+        List<trackingMemStream> writer; //this is a list so you can lock on it.
+        MemoryStream data;
 
-        public MemFSFile(string name, MemFSDir parentDir)
+        public MemFSFile(string name, MemFSDir parent)
         {
             this.fileName = name;
+            parentDir = parent;
+            readers = new List<trackingMemStream>() ;
+            writer = new List<trackingMemStream>(); 
+            data = new MemoryStream();
         }
 
-        public string Name
+        public MemoryStream mdata
+        {
+            get
+            {
+                return data;
+            }
+        }
+
+        public override string Name
         {
             get
             {
@@ -379,7 +438,7 @@ namespace CS422
             }
         }
 
-        public MemFSDir Parent
+        public override Dir422 Parent
         {
             get
             {
@@ -387,17 +446,157 @@ namespace CS422
             }
         }
 
-
-
         public override Stream OpenReadOnly() 
         {
-            throw new NotImplementedException();
+            lock (writer)
+            {
+                if (writer.Count == 0)
+                    return new trackingMemStream(false, this);
+                return null;
+            }
+               
+            
         }
 
         public override Stream OpenReadWrite()
         {
-            throw new NotImplementedException();
+           lock (writer)
+            {
+                if (writer.Count > 0 || readers.Count > 0) //don't create a writer if there is 1 reader or 1 writer
+                    return null;
+                else
+                {
+                    writer.Add(new trackingMemStream(true, this));
+                    return writer[0]; //writer is only ever size 1 or 0
+                }
+            }
         }
+
+        class trackingMemStream : MemoryStream //custom memory stream
+        {
+            MemoryStream actualStream;
+            private bool canWrite;
+            MemFSFile file;
+            long position;
+
+            public trackingMemStream(bool write, MemFSFile originFile) //constructor class
+            {
+                    actualStream = new MemoryStream();
+                    originFile.data.CopyTo(actualStream);
+                    actualStream.Position = 0;
+                    canWrite = write;
+                    file = originFile;
+                    position = 0;
+            }
+
+            public override bool CanWrite
+            {
+                get
+                {
+                    if (canWrite && actualStream.CanWrite)
+                        return true;
+                    else
+                        return false;
+                }
+            }
+
+            public override bool CanRead
+            {
+                get
+                {
+                    return actualStream.CanRead;
+                }
+            }
+
+            public override bool CanSeek
+            {
+                get
+                {
+                    return actualStream.CanSeek;
+                }
+            }
+
+            public override long Length
+            {
+                get
+                {
+                    return actualStream.Length;
+                }
+            }
+
+            public override long Position
+            {
+                get
+                {
+                    return actualStream.Position;
+                }
+
+                set
+                {
+                    actualStream.Position = value;
+                }
+            }
+
+            public override int Read(byte[] buffer, int offset, int count)
+            {
+                if (this.CanRead)
+                    return actualStream.Read(buffer, offset, count);
+                return 0;
+            }
+
+            public override void Write(byte[] buffer, int offset, int count) //write to this stream, and the data stream which holds onto data just like a file would
+            {
+                if (this.CanWrite)
+                {
+                    file.mdata.Position = position; //set the position of the other stream before writing
+                    actualStream.Write(buffer, offset, count);
+                    file.mdata.Write(buffer, offset, count);
+                    return;
+                }
+                    
+                else
+                    throw new NotSupportedException();
+            }
+
+            public override void Close()
+            {
+                
+                if (this.canWrite)
+                {
+                    lock (file.writer)
+                    {
+                        file.mdata.Position = 0;
+                        file.writer.Remove(this);
+                    }
+                    
+                }
+                else //reader
+                {
+                    lock (file.readers)
+                    {
+                        file.readers.Remove(this);
+                    }
+                }
+
+                actualStream.Close();
+                base.Close();
+            }
+
+            public override long Seek(long offset, SeekOrigin loc)
+            {
+                return actualStream.Seek(offset, loc);
+            }
+
+            public override void Flush()
+            {
+                actualStream.Flush();
+            }
+
+            public override void SetLength(long value)
+            {
+                actualStream.SetLength(value);
+            }
+        } //end special stream
     }
 }
 
